eb1f7eafc5d0d7adc78380daf43633c04e7f240e
Max Hardtke <Max-Tom@outlook.de>
2021-05-15 22:06:04 +0200

    + Added Payout statistics

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ dotenv.config();
 import {mainLogger} from "./utils/logger";
 import {getPrice} from "./utils/CoinbaseAPI";
 import {getPayouts, getPoolData, PayoutInfo, PoolInfo} from "./utils/HPoolAPI";
+import moment from "moment";
 
 const run = () => {
   mainLogger.debug("Obtaining Coinbase chia Price ...");
@@ -39,12 +40,22 @@ const run = () => {
             mainLogger.info(`Last Payout was ${hours.toFixed(2)} ago.`);
 
             const earningEstimate = uBalance * 24/hours;
-            mainLogger.info(`Estimated Earning (24h): ${earningEstimate} XCH x ${price}€ -> ${parseFloat((earningEstimate * price).toFixed(2))}€`);
+            const gain = res.data.list.length > 0 ? parseFloat(((earningEstimate/parseFloat(res.data.list[0].amount) - 1) * 100).toFixed(2)) : undefined;
+            const gainString = gain ? `${gain < 0 ? " " : "+ "} ${gain}%` : undefined;
+
+            mainLogger.info(`Estimated (24h):\t ${earningEstimate.toFixed(8)} XCH x ${price}€ -> ${parseFloat((earningEstimate * price).toFixed(2))}€ \t ${gain ? "| " + gainString : ""}`);
 
 
-            /*console.log();
-            mainLogger.info("Revenue Progress:");
-            let previous = 0;*/
+            console.log();
+            mainLogger.info("Past Revenues:");
+            //let previous = 0;
+
+            res.data.list.forEach((pi, i) => {
+              const amount = parseFloat(pi.amount);
+              const gain = res.data.list.length > i + 1 ? parseFloat(((amount/parseFloat(res.data.list[i+1].amount) - 1) * 100).toFixed(2)) : undefined;
+              const gainString = gain ? `${gain < 0 ? " " : "+ "} ${gain}%` : undefined;
+              mainLogger.info(`${moment(pi.record_time*1000).format("DD.MM.YYYY HH:mm:ss")}\t ${amount.toFixed(8)} XCH x ${price}€ -> ${parseFloat((amount * price).toFixed(2))}€ \t ${gain ? "| " + gainString : ""}`)
+            });
 
           } else {
             mainLogger.error("No chia Payouts found in your Account.");
